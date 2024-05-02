a310cbebb9270d46078804ae375898b4140584d7
Yash <yashsahane23@gmail.com>
2024-05-03 01:25:05 +0530

    reload fix

--- server/app.js
+++ server/app.js
@@ -18,3 +18,11 @@ config({
 app.use(cors())
 
 app.use('/users', userRouter);
+
+// Define route to serve React application's static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Define route to handle client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
