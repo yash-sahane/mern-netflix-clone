3b7912689a424f1b700ace32bdcc84fbabd9616b
Yash <yashsahane23@gmail.com>
2024-05-03 01:26:52 +0530

    realod fix

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from "dotenv";
 import userRouter from './routes/user.js';
 import cors from 'cors';
+import path from 'path'; // Import the 'path' module
 
 export const app = express();
 
@@ -9,13 +10,13 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello');
-})
+});
 
 config({
   path: './database/config.env'
 });
 
-app.use(cors())
+app.use(cors());
 
 app.use('/users', userRouter);
 
