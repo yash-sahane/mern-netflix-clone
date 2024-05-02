4f2845b189a49a3be34812fbb58b0a60546d7659
Yash <yashsahane23@gmail.com>
2024-05-03 01:04:40 +0530

    domain fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import './index.css';
 import store from './store.js';
 
-export const server = 'http://localhost:5000'
+export const server = 'https://mern-netflix-clone-am1c.onrender.com'
+// export const server = 'http://localhost:5000'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
